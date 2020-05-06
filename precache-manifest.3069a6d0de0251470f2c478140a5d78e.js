f35d758152b2be1cbff8827a77c126dd99d8ca4c
demi zhao <demizhao@demis-MacBook-Pro.local>
2020-05-06 16:27:31 -0400

    Updates

---
+++ precache-manifest.3069a6d0de0251470f2c478140a5d78e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749603fb17990e1233e84c110a4d3bc6",
+    "url": "/secure-react-auth0/index.html"
+  },
+  {
+    "revision": "798aef5ad6e062cc3471",
+    "url": "/secure-react-auth0/static/css/main.e4d93510.chunk.css"
+  },
+  {
+    "revision": "be2c0c23ae4ce34266d7",
+    "url": "/secure-react-auth0/static/js/2.2d1fe7b5.chunk.js"
+  },
+  {
+    "revision": "c5d0ae5c9031771b04372b2e4fc6a2c8",
+    "url": "/secure-react-auth0/static/js/2.2d1fe7b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798aef5ad6e062cc3471",
+    "url": "/secure-react-auth0/static/js/main.587507d6.chunk.js"
+  },
+  {
+    "revision": "9f7b6df2b2699e5339bc",
+    "url": "/secure-react-auth0/static/js/runtime-main.e0532dd5.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/secure-react-auth0/static/media/loading.c5590569.svg"
+  }
+]);
