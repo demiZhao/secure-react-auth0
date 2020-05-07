132724feaa118938c77e166194824a34d155cfd6
demi zhao <demizhao@demis-MacBook-Pro.local>
2020-05-06 21:12:06 -0400

    Fix config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
@@ -31,4 +26,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
